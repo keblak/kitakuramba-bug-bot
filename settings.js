67b7f1ff81a4bd78d1f090053b9aab2b7541a315
keblak <168828596+keblak@users.noreply.github.com>
2024-09-04 21:48:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,8 +31,8 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
-global.autoread = false //auto read messages
+global.autorecordtype = true //auto typing + recording
+global.autoread = true //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
